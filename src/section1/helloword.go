975084bcae6bf1e700eb2cdd0bc7a81f7e6f0c42
AllUneedisS2 <bmdk594@naver.com>
2025-08-20 14:38:01 +0900

    20250820-1438

--- src/section1/helloword.go
+++ src/section1/helloword.go
@@ -1,8 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	name := "Hello World"
 	fmt.Println(name, "from section1!")
+
+	// 1. int 타입 채널(stream) 생성
+	ch := make(chan int)
+
+	// 2. goroutine에서 값 보내기
+	go func() {
+		ch <- 42
+	}()
+
+	// 3. 채널에서 값 받기
+	val := <-ch
+	fmt.Println("Received from stream:", val)
 }
