7ca6b1af0516e1570f36dd441b62a4c1b82a9fb6
AllUneedisS2 <bmdk594@naver.com>
2026-01-22 13:21:47 +0900

    20260122-1321

--- src/section11/file_write1.go
+++ src/section11/file_write1.go
@@ -1,3 +1,4 @@
+//파일 쓰기(1)
 package main
 
 import (
@@ -5,46 +6,68 @@ import (
 	"os"
 )
 
+//에러 체크 방식1
 func errCheck1(e error) {
 	if e != nil {
 		panic(e)
 	}
 }
 
+//에러 체크 방식2
+
 func errCheck2(e error) {
 	if e != nil {
-		fmt.Println("Error:", e)
+		fmt.Println(e)
+		return
 	}
 }
 
 func main() {
+	//파일 쓰기
+	//Create : 새 파일 작성 및 파일 열기
+	//Close : 리소스 닫기
+	//Write, WriteString, WriteAt : 파일 쓰기
+	//각 운영체제 권한 주의(오류 메시지 확인)
+	//예외 처리 정말 중요!
+	//https://golang.org/pkg/os
 
+	//파일 쓰기 예제
 	file, err := os.Create("test_write.txt")
 	errCheck1(err)
 
+	//리소스 해제
 	defer file.Close()
 
-	s1 := []byte{7, 8, 9, 10, 11, 12}
-	n1, err := file.Write(s1)
+	//쓰기 예제1
+	s1 := []byte{48, 49, 50, 51, 52}
+	n1, err := file.Write([]byte(s1)) //문자열 -> byte 슬라이스 형으로 변환 후 쓰기
 	errCheck2(err)
-	fmt.Printf("쓰기(1) 완료: %d byte\n", n1)
 
-	file.Sync() // 파일 버퍼 비우기
+	fmt.Printf("쓰기 작업(1) 완료 (%d bytes) \n", n1)
 
-	s2 := "Hello Go Lang!!\n"
+	file.Sync() //Write Commit(Stable)!
+
+	//쓰기 예제2
+	s2 := "\nHello Golang!\n File Write Test! - 1\n"
 	n2, err := file.WriteString(s2)
 	errCheck2(err)
-	fmt.Printf("쓰기(2) 완료: %d byte\n", n2)
 
-	file.Sync() // 파일 버퍼 비우기
-	
-	s3 := "안녕하세요!!\n"
-	n3, err := file.WriteAt([]byte(s3), 70)
+	fmt.Printf("쓰기 작업(2) 완료 (%d bytes) \n", n2)
+
+	file.Sync() //Write Commit(Stable)!
+
+	//쓰기 예제3
+	s3 := "Test WriteAt! -2\n"
+	n3, err := file.WriteAt([]byte(s3), 100) //len(offset) 조절하면서 테스트
 	errCheck1(err)
-	fmt.Printf("쓰기(3) 완료: %d byte\n", n3)
-	
-	file.Sync() // 파일 버퍼 비우기
 
+	fmt.Printf("쓰기 작업(3) 완료 (%d bytes) \n", n3)
 
-	
+	file.Sync()
+
+	//쓰기 예제4
+	n4, err := file.WriteString("Hello Golang! \n File Write Test! - 3\n")
+	errCheck1(err)
+
+	fmt.Printf("쓰기 작업(4) 완료 (%d bytes) \n", n4)
 }
